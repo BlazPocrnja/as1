4dde9dc750d4c1b4bc6dff10f47d5862a1a0abb1
BlazPocrnja <bpocrnja@gmail.com>
2016-09-27 16:19:10 -0600

    Finished Habit, Changed some layouts

--- HabitTracker/app/src/main/java/example/habittracker/Habit.java
+++ HabitTracker/app/src/main/java/example/habittracker/Habit.java
@@ -1,54 +1,104 @@
 package example.habittracker;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 
 /**
  * Created by pocrn_000 on 9/26/2016.
  */
 public class Habit {
 
+    //--------Private Data-----------
     private String name;
-    private Date date;
-    private ArrayList<Date> completions;
+    private Calendar date;
 
+    //7 values to toggle specific days of the week; correspond to
+    // 0 , 1 ,  2 , 3 ,  4  , 5 , 6
+    //Sun,Mon,Tues,Wed,Thurs,Fri,Sat
+    private boolean[] days;
+
+    //Array of dates when this habit was completed
+    private ArrayList<Calendar> completions;
+
+
+    //--------Constructors-----------
     public Habit(String name){
         this.name = name;
-        this.date = new Date();
-        this.completions = new ArrayList<Date>();
+        this.date = Calendar.getInstance();
+        this.completions = new ArrayList<Calendar>();
+        this.days = new boolean[7];
     }
 
-    public Habit(String name, Date date){
+    public Habit(String name, Calendar date){
         this.name = name;
         this.date = date;
-        this.completions = new ArrayList<Date>();
+        this.completions = new ArrayList<Calendar>();
+        this.days = new boolean[7];
     }
 
-    public void setName(String name){
-        if(name.length() > 25){
 
-            //To do
+    //-----------Setters----------------
+    //Sets boolean for a specific day
+    public void setDays(int i, boolean b){
+            this.days[i] = b;
+    }
+
+    public void setName(String name) throws NameTooLongException{
+        if (name.length() > 25) {
+            this.name = name.substring(0,25);
+            throw new NameTooLongException();
         }
-        this.name = name;
+        else this.name = name;
     }
 
-    public void setDate(Date date){
+    public void setDate(Calendar date){
         this.date = date;
+    }
+
+    public void addCompletion(Calendar date){
+        this.completions.add(date);
+    }
+
+    //Removes first element with specified date(which are unique to the millisecond)
+    public void removeCompletion(Calendar date){
+        this.completions.remove(date);
+    }
+
+
+    //-----------Getters----------------
+    //Returns a value of specific day
+    public boolean getDays(int i){
+        return days[i];
     }
 
     public String getName(){
         return this.name;
     }
 
-    public Date getDate(){
+    public Calendar getDate(){
         return this.date;
     }
 
-    public void addCompletion(Date date){
-        completions.add(date);
+    //Returns a copy of the list of completion dates
+    //Source: http://stackoverflow.com/questions/7042182/how-to-make-a-deep-copy-of-java-arraylist
+    public ArrayList<Calendar> getCompletions(){
+        ArrayList<Calendar> tmp = new ArrayList<Calendar>();
+        for(Calendar c : this.completions){
+            tmp.add((Calendar) c.clone());
+        }
+        return tmp;
     }
 
-    public void getCompletion(){
-
+    //Checks if habit has been completed at least once
+    public boolean isComplete(){
+        if(this.completions.isEmpty()){
+            return false;
+        }
+        return true;
     }
+
+
+
+
+
 }
---
+++ HabitTracker/app/src/main/java/example/habittracker/NameTooLongException.java
@@ -0,0 +1,7 @@
+package example.habittracker;
+
+/**
+ * Created by pocrn_000 on 9/27/2016.
+ */
+public class NameTooLongException extends Exception {
+}
