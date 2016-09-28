7d51b06bb6718617ffcd421f8ecb195a75a6fec3
BlazPocrnja <bpocrnja@gmail.com>
2016-09-28 01:20:24 -0600

    Layouts made

---
+++ HabitTracker/app/src/main/java/example/habittracker/AddHabitActivity.java
@@ -0,0 +1,19 @@
+package example.habittracker;
+
+import android.os.Bundle;
+
+/**
+ * Created by pocrn_000 on 9/27/2016.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_habit);
+    }
+}
---
+++ HabitTracker/app/src/main/java/example/habittracker/AllHabitsActivity.java
@@ -0,0 +1,16 @@
+package example.habittracker;
+
+/**
+ * Created by pocrn_000 on 9/28/2016.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllHabitsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_habits);
+    }
+}
