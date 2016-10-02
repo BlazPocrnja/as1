65faa6d1f2325f3de72c7f05ba02fe7cc36f6d0c
BlazPocrnja <bpocrnja@gmail.com>
2016-10-02 00:59:20 -0600

    Fixed FileTest

--- HabitTracker/app/src/androidTest/java/example/habittracker/FileTest.java
+++ HabitTracker/app/src/androidTest/java/example/habittracker/FileTest.java
@@ -16,24 +16,28 @@ public class FileTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public void testSave(){
         ArrayList<Habit> habits = new ArrayList<Habit>();
         Habit habit = new Habit("Unique Name");
+        habits.add(habit);
 
-        Context context = getInstrumentation().getContext();
+        Context context = getActivity();
+
         FileRetriever retriever = new FileRetriever(context);
-        retriever.saveInFile(habits, "TestFile");
+        retriever.saveInFile(habits, "TestFile.sav");
 
         ArrayList<Habit> alsoHabits = new ArrayList<Habit>();
-        alsoHabits = retriever.loadFromFile("TestFile");
+        alsoHabits = retriever.loadFromFile("TestFile.sav");
 
-        assertTrue(alsoHabits.get(0).equals("Unique Name"));
+        assertTrue(alsoHabits.get(0).getName().equals("Unique Name"));
     }
 
     public void testLoad(){
         ArrayList<Habit> moreHabits = new ArrayList<Habit>();
 
-        Context context = getInstrumentation().getContext();
+        Context context = getActivity();
         FileRetriever retriever = new FileRetriever(context);
-        moreHabits = retriever.loadFromFile("TestFile");
+        moreHabits = retriever.loadFromFile("TestFile.sav");
 
-        assertTrue(moreHabits.get(0).equals("Unique Name"));
+        assertTrue(moreHabits.get(0).getName().equals("Unique Name"));
     }
+
+
 }
